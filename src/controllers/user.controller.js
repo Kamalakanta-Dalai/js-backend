782c0ae6408a1cd57000f68ae9acb0aabd9b56ff
Kamalakanta Dalai <dalaikamalakanta52@gmail.com>
2024-01-12 11:06:32 +0530

    One more

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    Message: "ok",
+  res.status(200).json({
+    message: "ok",
   });
 });
 
